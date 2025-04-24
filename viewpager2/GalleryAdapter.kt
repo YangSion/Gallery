04faacdd236b73d3e99ca4b2fb6d16db76316561
YangSion <yangsion@126.com>
2025-04-24 10:46:34 +0800

    Update GalleryAdapter.kt

--- viewpager2/GalleryAdapter.kt
+++ viewpager2/GalleryAdapter.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
-import com.smtlink.testdemokt.R
+import com.xxx.xxx.R
 
 class GalleryAdapter(private val items: List<Info>) : RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
 
